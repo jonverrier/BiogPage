a735f31db204e7c6cf1e3b102344c37d39374eb9
Jon <jonathan.verrier@hotmail.com>
2023-01-30 16:35:17 +0000

    Try to rad secret

--- api/key/index.js
+++ api/key/index.js
@@ -1,13 +1,21 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+
+module.exports = async function (context, req) {
+
+   const credential = new DefaultAzureCredential();
+
+   const url = `https://FluidClient.vault.azure.net`;
+
+   const client = new SecretClient(url, credential);
+
+   const secretName = "FluidSecret";
+
+   const latestSecret = await client.getSecret(secretName);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-       body: responseMessage + "" + process.env.testKey
+       body: latestSecret
     };
 }
