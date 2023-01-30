74be5077972031fa0409ef4a7f91937d757526f5
Jon <jonathan.verrier@hotmail.com>
2023-01-30 17:20:19 +0000

    logging

--- api/key/index.js
+++ api/key/index.js
@@ -1,9 +1,8 @@
 
-const { DefaultAzureCredential } = require("@azure/identity");
-const { SecretClient } = require("@azure/keyvault-secrets");
 
 module.exports = async function (context, req) {
 
+   context.log('One.');
    const credential = new DefaultAzureCredential();
 
    const url = `https://FluidClient.vault.azure.net`;
@@ -12,9 +11,12 @@ module.exports = async function (context, req) {
 
    const secretName = "FluidSecret";
 
+   context.log('Two.');
    const latestSecret = await client.getSecret(secretName);
 
-    context.res = {
+   context.log('Three.');
+
+   context.res = {
         // status: 200, /* Defaults to 200 */
        body: latestSecret
     };
