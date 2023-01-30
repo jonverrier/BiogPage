6dedfb8fc5c376e50dacdc5a0d8830a7e595dca1
Jon <jonathan.verrier@hotmail.com>
2023-01-30 20:43:20 +0000

    Just read key from environment.

--- api/key/index.js
+++ api/key/index.js
@@ -2,22 +2,9 @@
 
 module.exports = async function (context, req) {
 
-   context.log('One.');
-   const credential = new DefaultAzureCredential();
-
-   const url = `https://FluidClient.vault.azure.net`;
-
-   const client = new SecretClient(url, credential);
-
-   const secretName = "FluidSecret";
-
-   context.log('Two.');
-   const latestSecret = await client.getSecret(secretName);
-
-   context.log('Three.');
 
    context.res = {
-        // status: 200, /* Defaults to 200 */
-       body: latestSecret
+       /* Defaults to status 200 */
+       body: process.env.FluidRelayKey
     };
 }
